0b4403825bf0aef175c0f103896ed6510bf98a98
Gabriele Cardosi <gabriele.cardosi@tin.it>
2011-05-03 16:06:56 +0200

    Android Project Creation

---
+++ src/it/jugancona/jugsoftses/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.jugancona.jugsoftses;
+
+import android.app.Activity;
+
+/**
+ * Main.java generated 03/mag/2011
+ *
+ * (type in general information)
+ */
+
+/**
+ * @author Gabriele Cardosi
+ *
+ */
+public class Main extends Activity {
+
+}
